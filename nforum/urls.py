7724bd3171ede2ef2a71be5d47b5f027d641e237
duplxey <nick.tomazic@gmail.com>
2019-09-28 19:51:07 +0200

    Started home app

--- nforum/urls.py
+++ nforum/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('wiki/', include('wiki.urls')),
     path('forum/', include('forum.urls')),
     path('admin/', admin.site.urls),
