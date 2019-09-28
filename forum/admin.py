820a009270de4aa670cf5931b060d1f8d4fa9d3f
duplxey <nick.tomazic@gmail.com>
2019-09-28 23:06:07 +0200

    Created Category, Subcategory model

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subcategory, Category
+
+
+admin.site.register(Subcategory)
+admin.site.register(Category)
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Subcategory(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=250)
+
+    class Meta:
+        verbose_name_plural = "subcategories"
+
+    def __str__(self):
+        return self.title + " (" + self.description + ")"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=250)
+    subcategories = models.ManyToManyField(Subcategory, blank=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.title + " (" + self.description + ")"
