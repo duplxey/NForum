9d9ff172bdf3ca5a6dc41f25b0590f40633d3dda
duplxey <nick.tomazic@gmail.com>
2019-09-28 18:49:16 +0200

    Created thread representation

--- forum/views.py
+++ forum/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'forum/index.html', {})
+
+
+def thread(request):
+    return render(request, 'forum/thread.html', {})
