b15033c6a87176d2bb76fba9ff3f8bfcf6118255
duplxey <nick.tomazic@gmail.com>
2019-09-28 20:13:23 +0200

    Links now work properly

--- forum/urls.py
+++ forum/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('thread', views.thread, name='thread'),
-    path('subcategory', views.subcategory, name='subcategory')
+    path('', views.index, name='forum-index'),
+    path('thread', views.thread, name='forum-thread'),
+    path('subcategory', views.subcategory, name='forum-subcategory')
 ]
--- wiki/urls.py
+++ wiki/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('page', views.page, name='page'),
+    path('', views.index, name='wiki-index'),
+    path('page', views.page, name='wiki-page'),
 ]
