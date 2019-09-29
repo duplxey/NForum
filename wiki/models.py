d67e3ee2e076f929d25d60f0e6e0aa032da4cb0b
duplxey <nick.tomazic@gmail.com>
2019-09-29 10:10:51 +0200

    Created the WikiPage model

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class WikiPage(models.Model):
+    display_index = models.IntegerField()
+    title = models.CharField(max_length=64)
+    content = models.TextField(max_length=2500)
+    # TODO: add the author
+    # TODO: add the editors
+    created_datetime = models.DateTimeField(default=timezone.now)
+    edited_datetime = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- wiki/urls.py
+++ wiki/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='wiki-index'),
-    path('page', views.page, name='wiki-page'),
+    path('', views.page, name='wiki-page'),
 ]
