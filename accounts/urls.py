4eb310cabf63bc7a2005651d674c0b88e8e19ea9
duplxey <nick.tomazic@gmail.com>
2019-09-30 21:25:49 +0200

    Added authentication system

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='accounts-login'),
+    path('login/', views.login_view, name='accounts-login'),
     path('signup/', views.signup, name='accounts-signup'),
-    path('logout/', views.logout, name='accounts-logout')
+    path('logout/', views.logout_view, name='accounts-logout'),
+    path('home/', views.home, name='accounts-home'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,23 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 from accounts.forms import LoginForm, SignupForm
 
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            return render(request, 'accounts/login.html', {'form': form})
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return render(request, 'accounts/home.html', {'form': form})
+            else:
+                return render(request, 'accounts/login.html', {'form': form})
         else:
             return render(request, 'accounts/login.html', {'form': form})
     else:
@@ -19,6 +29,22 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            confirm_password = form.cleaned_data['confirm_password']
+
+            if password != confirm_password:
+                form.add_error('confirm_password', "Passwords do not match!")
+                return render(request, 'accounts/signup.html', {'form': form})
+
+            if User.objects.filter(username=username).exists():
+                form.add_error('username', "User with this username already exists.")
+                return render(request, 'accounts/signup.html', {'form': form})
+
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+
             return render(request, 'accounts/signup.html', {'form': form})
         else:
             return render(request, 'accounts/signup.html', {'form': form})
@@ -27,5 +53,13 @@ def signup(request):
     return render(request, 'accounts/signup.html', {'form': form})
 
 
-def logout(request):
+def logout_view(request):
+    logout(request)
     return render(request, 'accounts/logout.html', {})
+
+
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'accounts/home.html', {})
+    else:
+        return render(request, 'home/index.html', {})
