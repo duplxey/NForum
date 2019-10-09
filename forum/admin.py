a24ced8a9bfa0c5a0c2eee4d0d42d8db78e2e431
duplxey <nick.tomazic@gmail.com>
2019-10-09 14:25:44 +0200

    Created the thread + message model

--- forum/admin.py
+++ forum/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Subcategory, Category
+from .models import *
 
 
 admin.site.register(Subcategory)
 admin.site.register(Category)
+admin.site.register(Message)
+admin.site.register(Thread)
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -22,3 +23,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title + " (" + self.description + ")"
+
+
+class Thread(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    content = models.TextField(max_length=2500)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True, blank=True)
+    date_edited = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.author.username + ":" + self.content
