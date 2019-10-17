3ce675a86aa21471b6d833db19ff3e1b04bd759d
duplxey <nick.tomazic@gmail.com>
2019-10-17 20:47:53 +0200

    Reply is now not submitted on refresh (F5)

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from accounts.models import Profile
@@ -63,7 +64,7 @@ def thread_post_view(request, thread_title):
                 message = Message.objects.create(thread=thread, content=content, author=request.user)
                 message.save()
 
-                return render(request, 'forum/thread.html', {'form': PostReplyForm(), 'thread': Thread.objects.get(title=thread_title)})
+                return HttpResponseRedirect(thread_title)
             else:
                 form = PostReplyForm()
                 return render(request, 'forum/thread.html', {'form': form, 'thread': Thread.objects.get(title=thread_title)})
