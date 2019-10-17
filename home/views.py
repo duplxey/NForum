deb0d09fa712b7b4eb99074fc936e3ceeceff143
duplxey <nick.tomazic@gmail.com>
2019-10-17 22:06:57 +0200

    Improvements...

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 
 def index_view(request):
-    return render(request, 'home/index.html', {})
+    return render(request, 'home/index.html', {'user_count': User.objects.all().count()})
