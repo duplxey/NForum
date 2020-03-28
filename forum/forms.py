beb7cbcbc34e0e6555ebb7e112fb81195ac6994a
duplxey <nick.tomazic@gmail.com>
2020-03-28 17:08:03 +0100

    Form content bump.

--- forum/forms.py
+++ forum/forms.py
@@ -7,11 +7,11 @@ from forum.models import ThreadPrefix
 class CreateThreadForm(forms.Form):
     prefix = forms.ModelChoiceField(queryset=ThreadPrefix.objects.all(), required=False)
     title = forms.CharField(label="Thread title", max_length=200)
-    content = forms.CharField(widget=TinyMCE(attrs={'cols': 40, 'rows': 15}), max_length=2500)
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 40, 'rows': 15}), max_length=25000)
 
 
 class PostReplyForm(forms.Form):
-    content = forms.CharField(widget=TinyMCE(attrs={'cols': 40, 'rows': 15}), max_length=2500)
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 40, 'rows': 15}), max_length=25000)
 
 
 class PostDeleteForm(forms.Form):
