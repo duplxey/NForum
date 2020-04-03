aa3264e66ec9a9997e71753be6e35d8e59d1476b
duplxey <nick.tomazic@gmail.com>
2020-04-03 22:18:54 +0200

    Home now displays selected category's threads

--- forum/views.py
+++ forum/views.py
@@ -14,8 +14,19 @@ from .models import *
 
 
 def home_view(request):
+    forum_config = ForumConfiguration.get_solo()
+
+    threads = []
+    if forum_config.home_category:
+        threads = reversed(Thread.objects.filter(subcategory__category=forum_config.home_category))
+
     return render(request, 'forum/home.html', {
-        'categories': Category.objects.all(),
+        'threads': threads,
+        'recent_messages': Message.get_recent_messages(5),
+        'thread_count': Thread.objects.count(),
+        'message_count': Message.objects.count(),
+        'registered_user_count': User.objects.count(),
+        'active_user_count': User.objects.filter(is_active=True).count(),
     })
 
 
