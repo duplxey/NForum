a4dec5de34d95e8842fcda306193a92c39bb3665
duplxey <nick.tomazic@gmail.com>
2020-04-04 16:53:36 +0200

    SEO: title (+ subtitle), keywords, description, short_description, favicon.

--- home/models.py
+++ home/models.py
@@ -6,6 +6,8 @@ class SiteConfiguration(SingletonModel):
     name = models.CharField(max_length=255, default="Site Name")
     description = models.TextField(max_length=750, default="Site Description")
     description_short = models.TextField(max_length=128, default="Site Short Description")
+    keywords = models.TextField(max_length=750, default="keyword1, keyword2, keyword3, keyword4")
+    favicon = models.ImageField(null=True, blank=True)
     logo = models.ImageField(null=True, blank=True)
     maintenance_mode = models.BooleanField(default=False)
 
